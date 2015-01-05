522a2e89645723934aa984fcdf9da9f3e104d19e
Matteo Capucci <mattecapu@live.it>
2015-01-05 22:30:28 +0100

    main features implementation

--- source/main.js
+++ source/main.js
@@ -1,5 +1,26 @@
-var $canvas = $(".canvas");
+$(() => {
+	var $paper = $(".paper");
+	var $selected = null;
+	var id = 1;
 
-$canvas.click((event) => {
-	console.log(event);
-})
+	$paper.click((event) => {
+
+		if ($selected) $selected.removeClass('selected');
+
+		$selected = $(document.elementFromPoint(event.clientX, event.clientY));
+
+		if (!$selected.hasClass('note')) {
+			$selected = $('<textarea class="note"></textarea>');
+			$selected
+				.css({top: event.clientY - 10 + $(window).scrollTop(), left: event.clientX + $(window).scrollLeft()})
+				.attr('data-id', id++)
+				.appendTo($paper);
+		}
+
+		$selected.addClass('selected').focus();
+
+		console.log(event.clientX, event.clientY, $selected);
+	});
+	
+	$(window).keyup(() => $paper.focus());
+});
